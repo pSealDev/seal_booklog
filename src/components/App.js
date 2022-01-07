cda4981ba68db1b04d7d202c2a36d6b98d227c95
pSealDev <priscilla.seal.tech@gmail.com>
2022-01-06 21:46:33 -0800

    rearranged styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import MainGrid from './MainGrid'
-import './App.css'
+import '../styles/App.css'
 import BookFunctions from '../services/BookFunctions'
 
 
 function App() {
-
   return (
     <BookFunctions />
   )
